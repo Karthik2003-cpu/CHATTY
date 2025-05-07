6b19fce3c5e27efe3cbecfb666ec4606de67fa4b
Karthik2003-cpu <karthikrckm2003@gmail.com>
2025-05-07 16:43:44 +0530

    first commit

---
+++ backend/src/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import { connectDB } from "./lib/db.js";
+import cookieParser from 'cookie-parser';
+import cors from "cors";
+import { app, server } from "./lib/socket.js";
+
+
+dotenv.config();
+
+
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}))
+
+app.use('/api/auth', authRoutes);
+app.use('/api/message', messageRoutes);
+
+const port = process.env.PORT;
+
+server.listen(port, () => {
+    console.log("Server is running on port: "+port);
+    connectDB();
+});
